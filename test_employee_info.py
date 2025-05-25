580fa283a51817f28d3e16c4b22668a6cbb7d99f
Irwin <irwinlzx99.24@ichat.sp.edu.sg>
2025-05-25 15:10:41 +0800

    Completed Ex5

---
+++ test_employee_info.py
@@ -0,0 +1,26 @@
+import employee_info 
+
+def test_get_employee_by_age_range():
+    test = [
+    {"name": "Jane", "age": 25, "department": "Marketing", "salary": 60000},
+    {"name": "Mary", "age": 23, "department": "Marketing", "salary": 56000}
+    ]
+
+    result = employee_info.get_employees_by_age_range(20, 26)
+
+    assert result == test
+
+def test_calculate_average_salary():
+    result = employee_info.calculate_average_salary()
+
+    assert result == 60166.67 
+
+def test_get_employees_by_dept():
+    test = [
+    {"name": "Chloe",  "age": 35, "department": "Engineering", "salary": 70000},
+    {"name": "Mike", "age": 32, "department": "Engineering", "salary": 65000}
+    ]
+
+    result = employee_info.get_employees_by_dept("Engineering")
+
+    assert result == test
